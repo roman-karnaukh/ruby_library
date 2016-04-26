f3d2af15371fadd149064775b30f7222603b9f71
roman-karnaukh <roman.karnaukh@gmail.com>
2016-04-26 12:31:56 +0300

    delete spaces

--- basic.rb
+++ basic.rb
@@ -1,56 +1,55 @@
-		require 'csv'
+require 'csv'
 
-		module Basic
+module Basic
 
-			def class_name 
-				self.class.name
-			end
+  def class_name
+	  self.class.name
+  end
 
-			def load_to_array(array, file)
-				CSV.foreach("./data/#{file}") do |line| 
-					next if array.include? line 
-					array.push line
-				end
-			end
-			
-			def i_want_to_open(file_name)
-				puts file_name[/[a-z]+/].capitalize
-				read(file_name).each {|row| p row }
-			end
+  def load_to_array(array, file)
+	  CSV.foreach("./data/#{file}") do |line|
+    next if array.include? line
+		 array.push line
+	  end
+  end
 
-			def variable
-				instance_variable_get(instance_variables.inspect[/@[a-z]+/])
-			end
+  def i_want_to_open(file_name)
+  	puts file_name[/[a-z]+/].capitalize
+    read(file_name).each {|row| p row }
+  end
 
-			def file
-				"#{class_name.downcase}s.csv"
-			end
+  def variable
+    instance_variable_get(instance_variables.inspect[/@[a-z]+/])
+  end
 
-			def read(file, headers = true)
-				CSV.read("./data/#{file}", headers:headers)
-			end
+  def file
+	  "#{class_name.downcase}s.csv"
+  end
+
+  def read(file, headers = true)
+	  CSV.read("./data/#{file}", headers:headers)
+  end
 
 
-			def save
-				@readed_file = read(file)
-				if @readed_file.to_a.include? variable
-					puts "#{class_name} #{variable} already exists"
-				else
-					CSV.open("./data/#{file}", 'a+') { |file| file << variable }
-					puts "Entered #{class_name.downcase}: #{variable} saved"
-				end
-			end
+  def save
+  	@readed_file = read(file)
+    if @readed_file.to_a.include? variable
+      puts "#{class_name} #{variable} already exists"
+    else
+      CSV.open("./data/#{file}", 'a+') { |file| file << variable }
+      puts "Entered #{class_name.downcase}: #{variable} saved"
+    end
+  end
 
-			def delete
-				@readed_file = read(file)
-				if @readed_file.to_a.include? variable
-					CSV.open("./data/#{file}", 'w') do |csv|
-					    @readed_file.to_a.each { |a| next if a == variable; csv << a}
-					end
-					puts "#{class_name} #{variable} deleted"
-				else
-					puts "There is no #{class_name.downcase}: #{variable} you want to delete"
-				end
-			end
-
-		end
+  def delete
+    @readed_file = read(file)
+    if @readed_file.to_a.include? variable
+      CSV.open("./data/#{file}", 'w') do |csv|
+      @readed_file.to_a.each { |a| next if a == variable; csv << a}
+    end
+      puts "#{class_name} #{variable} deleted"
+    else
+      puts "There is no #{class_name.downcase}: #{variable} you want to delete"
+    end
+  end
+end
--- library.rb
+++ library.rb
@@ -1,73 +1,73 @@
-		require_relative 'basic'
-		require_relative 'author'
-		require_relative 'reader'
-		require_relative 'order'
-		require_relative 'book'
-		require 'csv'
+require_relative 'basic'
+require_relative 'author'
+require_relative 'reader'
+require_relative 'order'
+require_relative 'book'
+require 'csv'
 
-		class Library
-			include Basic
-			attr_reader :books, :orders, :readers, :authors
-			
-			def library_open
-				@books = []
-				@orders = []
-				@readers = []
-				@authors = []
-				load_to_array(@books, "books.csv")
-				load_to_array(@authors, "authors.csv")
-				load_to_array(@readers, "readers.csv")
-				load_to_array(@orders, "orders.csv")
-			end
+class Library
+	include Basic
+	attr_reader :books, :orders, :readers, :authors
 
-			def reader_read_book(reader_name, book_title)
-				if @books.to_s.include? book_title
-					if @readers.to_s.include? reader_name
-						CSV.open('./data/library_register.csv', 'a+') {|file| file << [reader_name,book_title]}
-						puts "Record #{[reader_name,book_title]} saved"
-					else
-						p "Error - there is no reader name you entered"
-					end
-				else
-					p "Error - there is no book you selected"
-				end
-			end
+	def library_open
+	  @books = []
+	  @orders = []
+	  @readers = []
+	  @authors = []
+	  load_to_array(@books, "books.csv")
+	  load_to_array(@authors, "authors.csv")
+	  load_to_array(@readers, "readers.csv")
+	  load_to_array(@orders, "orders.csv")
+	end
 
-			def the_most_popular_book
-				@popular_book = load_with_raiting.select { |title, frequency|  @books.to_s.include? title }
-				puts "The most popular book is #{@popular_book.first[0]}, it`s readed #{@popular_book.first[1]} times"
-			end
+	def reader_read_book(reader_name, book_title)
+	  if @books.to_s.include? book_title
+		  if @readers.to_s.include? reader_name
+		    CSV.open('./data/library_register.csv', 'a+') {|file| file << [reader_name,book_title]}
+		    puts "Record #{[reader_name,book_title]} saved"
+		  else
+			  p "Error - there is no reader name you entered"
+		  end
+	  else
+		  p "Error - there is no book you selected"
+	  end
+	end
 
-			def the_most_frequent_reader
-				@frequent_reader = load_with_raiting.select { |name, frequency|  @readers.to_s.include? name }
-				puts "The most frequent reader is #{@frequent_reader.first[0]}, had readed #{@frequent_reader.first[1]} books"
-			end
+	def the_most_popular_book
+	  @popular_book = load_with_raiting.select { |title, frequency|  @books.to_s.include? title }
+	  puts "The most popular book is #{@popular_book.first[0]}, it`s readed #{@popular_book.first[1]} times"
+	end
 
-			def three_most_popular_books
-				@three_most_popular_books = load_with_raiting.select { |title, frequency|  @books.to_s.include? title }
-				@books_raiting = []
-				@three_most_popular_books.take(3).each {|book, raiting| @books_raiting.push book}
-				put_readers_by_book_index(rand(0..2))
-			end
+	def the_most_frequent_reader
+	  @frequent_reader = load_with_raiting.select { |name, frequency|  @readers.to_s.include? name }
+	  puts "The most frequent reader is #{@frequent_reader.first[0]}, had readed #{@frequent_reader.first[1]} books"
+	end
 
-			private
-			def load_with_raiting
-				words = read('library_register.csv').to_s.split(/[\n,'\n]/)
-				frequencies = Hash.new(0)
-				words.each { |word| frequencies[word] += 1 }
-				frequencies = frequencies.sort_by {|name, raiting| raiting }
-				frequencies.reverse!
-			end
+	def three_most_popular_books
+	  @three_most_popular_books = load_with_raiting.select { |title, frequency|  @books.to_s.include? title }
+	  @books_raiting = []
+	  @three_most_popular_books.take(3).each {|book, raiting| @books_raiting.push book}
+		put_readers_by_book_index(rand(0..2))
+	end
 
-			def put_readers_by_book_index(index)
-				puts "Readers who order one of the tree most readed books: #{@books_raiting[index]}"
-				@most_popular_book_readers = []
-				@orders.each {|title, reader, date| @most_popular_book_readers.push reader if title == @books_raiting[index] }
-				@most_popular_book_readers.uniq.each {|name| p name}
-			end
-		end
-   
-=begin
+	private
+	def load_with_raiting
+	  words = read('library_register.csv').to_s.split(/[\n,'\n]/)
+	  frequencies = Hash.new(0)
+	  words.each { |word| frequencies[word] += 1 }
+	  frequencies = frequencies.sort_by {|name, raiting| raiting }
+	  frequencies.reverse!
+	end
+
+	def put_readers_by_book_index(index)
+	  puts "Readers who order one of the tree most readed books: #{@books_raiting[index]}"
+	  @most_popular_book_readers = []
+	  @orders.each {|title, reader, date| @most_popular_book_readers.push reader if title == @books_raiting[index] }
+	  @most_popular_book_readers.uniq.each {|name| p name}
+	end
+end
+
+
 		library_object = Library.new
 		library_object.library_open
 
@@ -96,4 +96,4 @@
 		reader_object.save
 		reader_object.delete
 
-=end
+
